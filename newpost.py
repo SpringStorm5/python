2d96a5150c3c384e0a9ee9374545f1e260e3fbac
SpringStorm5 <springstorm666@gmail.com>
2014-04-06 18:33:01 +0300

    изучаю python

---
+++ newpost.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import webbrowser
+a = 'http://dnevnik-galina.diary.ru/?newpost#vbform'
+b = 'http://www.livejournal.com/update.bml'
+c = 'http://www.tumblr.com/new/text'
+webbrowser.open(a)
+webbrowser.open(b)
+webbrowser.open(c)
---
+++ start.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+x = 15
+if (x<10) or (x>20):
+    print "x is bad"
+elif 10<x<20:
+     print "x is good!"
+else:
+    print "x is werid"
+
+
